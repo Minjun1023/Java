2a74f765548db5d1c005496e45eb1b718b1c06e9
minjunsong <songmj5000@naver.com>
2024-07-08 17:54:17 +0900

    자바 문법 공부

--- src/ch06/Car.java
+++ src/ch06/Car.java
@@ -1,30 +1,24 @@
 package ch06;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Car {
-    // 필드 선언
-    String company = "현대자동차";
+    //필드 선언
     String model;
-    String color;
-    int maxSpeed;
+    int speed;
 
-    // 생성자 선언
-    Car() {}
-
+    //생성자 선언
     Car(String model) {
-        // 20라인 생성자 호출
-        this.model = model;
+        this.model = model; //매개변수를 필드에 대입(this 생략 불가)
     }
 
-    Car(String model, String color) {
-        this.model = model;
-        this.color = color;
+    //메소드 선언
+    void setSpeed(int speed) {
+        this.speed = speed; //매개변수를 필드에 대입(this 생략 불가)
     }
 
-    Car(String model, String color, int maxSpeed) {
-        this.model = model;
-        this.color = color;
-        this.maxSpeed = maxSpeed;
+    void run() {
+        this.setSpeed(100);
+        System.out.println(this.model + "가 달립니다.(시속:" + this.speed + "km/h");
     }
-
-
 }
